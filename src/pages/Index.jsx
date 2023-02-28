ec84745030ea3e0fd8b9c6807926f98004c78466
LIGAMD\rcarmona <raymondcarmonatejeda01@gmail.com>
2023-02-28 16:41:56 -0400

    agregar tabla

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -12,6 +12,46 @@ const Index = () => {
   return (
     <>
       <h1 className="text-5xl text-blue-900 font-bold text-center">Clientes</h1>
+      <p className="mt-3 text-center font-bold">Administra tus Clientes</p>
+
+      {clientes.length ? (
+        <table className="w-full bg-white shadow-lg mt-5 table-auto">
+            <thead className="bg-blue-800 text-white">
+                <tr>
+                    <th className="p-2">Cliente</th>
+                    <th className="p-2">Contacto</th>
+                    <th className="p-2">Acciones</th>
+                    <th className="p-2">empresa</th>
+                </tr>
+            </thead>
+            <tbody >
+                {clientes.map( cliente => (
+                    <tr className="text-center ">
+                       <td className="p-2">
+                       {cliente.nombre}
+                        </td> 
+                       <td className="p-2">
+                       {cliente.telefono}
+                        </td> 
+                       <td className="p-2">
+                       {cliente.email}
+                        </td> 
+                        
+                       <td className="p-2">
+                       {cliente.empresa}
+                        </td> 
+                        
+                    </tr>
+
+
+                    
+                  
+                ))}
+            </tbody>
+        </table>
+      ):(
+        <p>Prueba</p>
+      )}
     </>
   );
 };
